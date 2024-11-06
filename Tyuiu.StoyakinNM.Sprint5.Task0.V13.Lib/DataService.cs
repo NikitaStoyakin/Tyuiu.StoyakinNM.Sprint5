e21fc448c3c3605721aa01910d198c670ad99326
Neority <Tyuiu.StoyakinNM@outlook.com>
2024-11-06 21:12:13 +0300

    Task0

--- Tyuiu.StoyakinNM.Sprint5.Task0.V13.Lib/DataService.cs
+++ Tyuiu.StoyakinNM.Sprint5.Task0.V13.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.StoyakinNM.Sprint5.Task0.V13.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
             x = 5;
             double z = -1.4 * Math.Pow(x, 3) + 2.3 * Math.Pow(x, 2) + 0.6 * x;
             z = Math.Round(z, 3);
