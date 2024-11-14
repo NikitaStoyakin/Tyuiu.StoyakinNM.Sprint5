fd14b957834c81b8662a8abe187841275fd42bf0
Neority <Tyuiu.StoyakinNM@outlook.com>
2024-11-14 13:52:02 +0300

    Task 5

---
+++ Tyuiu.StoyakinNM.Sprint5.Task5.V13.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.StoyakinNM.Sprint5.Task5.V13.Lib
+{
+    public class DataService : ISprint5Task5V13
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(' ');
+                    foreach (string value in values)
+                    {
+                        if (double.TryParse(value, out double number))
+                        {
+                            if (number > -1.5 && number < 1.5)
+                            {
+                                res += number;
+                            }
+                        }
+                    }
+                }
+            }
+            double result = res / 2;
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.StoyakinNM.Sprint5.Task5.V13/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.StoyakinNM.Sprint5.Task5.V13.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнил: Стоякин Н.М. | ПКТб-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #13                                                             *");
+        Console.WriteLine("* Выполнил: Стоякин Никита Михайлович | ПКТб 24-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Найти среднее значение всех чисел в файле, которые находятся в          *");
+        Console.WriteLine("* промежутке от -1.5 до 1.5. Полученный результат вывести на консоль.     *");
+        Console.WriteLine("* У вещественных значений округлить до трёх знаков после запятой.         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = Path.Combine(Path.GetTempPath(), "InPutDataFileTask5V13.txt");
+
+        Console.WriteLine(" Данные находятся в файле: " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double result = ds.LoadFromDataFile(path);
+
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
